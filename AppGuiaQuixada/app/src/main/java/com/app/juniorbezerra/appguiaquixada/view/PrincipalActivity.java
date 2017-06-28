88bd2c0da8feba8e771aaf423dcde6f6b5e95ec2
Junior Bezerra dos Santos <juniorbezerra38@gmail.com>
2017-06-28 14:43:55 -0300

    Butterknife

--- AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/PrincipalActivity.java
+++ AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/PrincipalActivity.java
@@ -15,38 +15,41 @@ import android.view.MenuItem;
 
 import com.app.juniorbezerra.appguiaquixada.R;
 
-public class PrincipalActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PrincipalActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ButterKnife.bind(this,PrincipalActivity.this);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -96,7 +99,6 @@ public class PrincipalActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
--- AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/SplashScreenActivity.java
+++ AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.app.juniorbezerra.appguiaquixada.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,13 @@ import com.app.juniorbezerra.appguiaquixada.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        startActivity(new Intent(this,PrincipalActivity.class));
+
     }
 }
