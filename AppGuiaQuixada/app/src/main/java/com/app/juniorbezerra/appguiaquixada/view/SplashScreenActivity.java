1987f254a02fb13a7e95d2af893711f17db15933
Junior Bezerra dos Santos <juniorbezerra38@gmail.com>
2017-06-28 14:11:19 -0300

    commit

--- AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/SplashScreenActivity.java
+++ AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/SplashScreenActivity.java
@@ -1,7 +1,9 @@
-package com.app.juniorbezerra.appguiaquixada;
+package com.app.juniorbezerra.appguiaquixada.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.app.juniorbezerra.appguiaquixada.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
