833f62f37079c455dec9ee16b13325705ef872bc
Junior Bezerra dos Santos <juniorbezerra38@gmail.com>
2017-06-29 09:07:09 -0300

    Imagens

--- AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/PrincipalActivity.java
+++ AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/PrincipalActivity.java
@@ -78,17 +78,7 @@ public class PrincipalActivity extends AppCompatActivity implements NavigationVi
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        if (id == R.id.nav_inicio) {
 
         }
 
