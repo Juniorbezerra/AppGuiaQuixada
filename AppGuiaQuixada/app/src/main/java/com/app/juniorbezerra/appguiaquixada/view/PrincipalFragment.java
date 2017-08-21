635df5523ed0fc9078f1fe173c4d8686198b2d37
Junior Bezerra dos Santos <juniorbezerra38@gmail.com>
2017-08-21 14:54:31 -0300

    Commit 2

--- AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/PrincipalFragment.java
+++ AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/PrincipalFragment.java
@@ -2,18 +2,21 @@ package com.app.juniorbezerra.appguiaquixada.view;
 
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
 import com.app.juniorbezerra.appguiaquixada.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PrincipalFragment extends Fragment {
-
 
     public PrincipalFragment() {
         // Required empty public constructor
@@ -21,10 +24,21 @@ public class PrincipalFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_principal, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_principal, container, false);
+
+        ButterKnife.bind(this,view);
+
+
+        return view;
+    }
+
+    @OnClick(R.id.sobre)
+    public void toast (View view){
+
+        Snackbar mySnackbar = Snackbar.make(getView(),"junior", Snackbar.LENGTH_SHORT);
+        mySnackbar.show();
+
     }
 
 }
--- AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/SplashScreenActivity.java
+++ AppGuiaQuixada/app/src/main/java/com/app/juniorbezerra/appguiaquixada/view/SplashScreenActivity.java
@@ -1,20 +1,29 @@
 package com.app.juniorbezerra.appguiaquixada.view;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.app.juniorbezerra.appguiaquixada.R;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity implements Runnable{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        getWindow().addFlags(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        Handler handler = new Handler();
+        handler.postDelayed(this,3000);
 
+    }
+
+    @Override
+    public void run() {
         startActivity(new Intent(this,PrincipalActivity.class));
-
+        finish();
     }
 }
